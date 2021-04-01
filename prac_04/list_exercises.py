85481aa1852f21ef80209aaf63e3231040ee31b4
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 13:09:32 +1100

    Display the smallest number in the list.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,3 +8,4 @@ for i in range(1, 6, 1):
 print()
 print("The first number is {}".format(numbers[0]))
 print("The last number is {}".format(numbers[4]))
+print("The smallest number is {}".format(min(numbers)))
