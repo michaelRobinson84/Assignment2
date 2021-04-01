0f2d319d95f1eeaf9710db836515d4de1aa37bc9
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 13:20:43 +1100

    Add woefully inadequate security checker.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,3 +12,14 @@ print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 average = sum(numbers) / 5
 print("The average of the numbers is {}".format(average))
+
+print()
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+user_name = input("Please enter your username: ")
+
+if user_name in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
