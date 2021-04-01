52c1a1c73933d66561717b591a258038d4e45cfd
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 16:18:04 +1100

    Finish quick_picks.py program.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -10,10 +10,16 @@ for number in range(0, num_of_picks, 1):
 
     random_numbers = []
 
-    repeated_number = True
-
     for random_number in range(0, NUM_OF_RANDOM_NUMS, 1):
+
+
         random_number = random.randint(1, HIGHEST_NUMBER)
+
+        while(random_number in random_numbers):
+
+            random_number = random.randint(1, HIGHEST_NUMBER)
+
+
         random_numbers.append(random_number)
 
     random_numbers.sort()
