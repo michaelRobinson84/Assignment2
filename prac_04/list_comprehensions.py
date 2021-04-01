a48f73a0ba765f84ca6cd2f694dec9596070a97f
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 14:10:40 +1100

    Update the TODO items in list_comprehensions.py

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -46,3 +46,7 @@ print(numbers)
 
 # TODO: use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+
+numbers_greater_than_9 = [number for number in numbers if number > 9]
+
+print(numbers_greater_than_9)
