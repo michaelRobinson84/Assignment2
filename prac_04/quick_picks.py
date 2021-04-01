5c3d4767f0196015731f490988c7996c8959bff0
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 16:27:23 +1100

    Clean up quick_picks.py code.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,7 +3,6 @@ import random
 NUM_OF_RANDOM_NUMS = 6
 HIGHEST_NUMBER = 45
 
-
 num_of_picks = int(input("How many quick picks? "))
 
 for number in range(0, num_of_picks, 1):
@@ -12,17 +11,13 @@ for number in range(0, num_of_picks, 1):
 
     for random_number in range(0, NUM_OF_RANDOM_NUMS, 1):
 
-
         random_number = random.randint(1, HIGHEST_NUMBER)
 
         while(random_number in random_numbers):
 
             random_number = random.randint(1, HIGHEST_NUMBER)
 
-
         random_numbers.append(random_number)
 
     random_numbers.sort()
     print(random_numbers)
-    # random_num = random.randint(1, HIGHEST_NUMBER)
-    # print(random_num)
