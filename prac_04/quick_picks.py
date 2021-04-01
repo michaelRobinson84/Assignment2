4bf1238050e6a5185aeb8d895b6dac7bf721aa9e
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 15:14:20 +1100

    Get program basically running.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,4 +3,22 @@ import random
 NUM_OF_RANDOM_NUMS = 6
 HIGHEST_NUMBER = 45
 
+
 num_of_picks = int(input("How many quick picks? "))
+
+for number in range(0, num_of_picks, 1):
+
+    random_numbers = []
+
+    for random_number in range(0, NUM_OF_RANDOM_NUMS, 1):
+        random_number = random.randint(1, HIGHEST_NUMBER)
+
+        if random_number in random_numbers:
+            pass
+        else:
+            random_numbers.append(random_number)
+
+    random_numbers.sort()
+    print(random_numbers)
+    # random_num = random.randint(1, HIGHEST_NUMBER)
+    # print(random_num)
