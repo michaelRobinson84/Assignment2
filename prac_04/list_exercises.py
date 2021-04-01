11d262c3c5288ea0721f1065bc427713828e78a2
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 13:13:33 +1100

    Display the average of the numbers in the list.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -10,3 +10,5 @@ print("The first number is {}".format(numbers[0]))
 print("The last number is {}".format(numbers[4]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
+average = sum(numbers) / 5
+print("The average of the numbers is {}".format(average))
