97f6791336b868a105f39a2809587f209af21694
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 15:21:58 +1100

    Get program basically running.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -10,13 +10,11 @@ for number in range(0, num_of_picks, 1):
 
     random_numbers = []
 
+    repeated_number = True
+
     for random_number in range(0, NUM_OF_RANDOM_NUMS, 1):
         random_number = random.randint(1, HIGHEST_NUMBER)
-
-        if random_number in random_numbers:
-            pass
-        else:
-            random_numbers.append(random_number)
+        random_numbers.append(random_number)
 
     random_numbers.sort()
     print(random_numbers)
