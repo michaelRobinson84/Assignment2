888860b8125894987583e38930114c401a6cca33
Mike <michael.robinson1@my.jcu.edu.au>
2021-04-01 13:07:24 +1100

    Display the first and last number in the list.

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,3 +4,7 @@ for i in range(1, 6, 1):
 
     user_input = int(input("Enter a number: "))
     numbers.append(user_input)
+
+print()
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[4]))
